fce82725daebdfabd30e705952712f0c26fe2618
sumit-zebpay <ssdas220496@gmail.com>
2022-07-03 13:54:44 +0530

    Added constants, strings & env

--- lib/constants/constant.dart
+++ lib/constants/constant.dart
@@ -1,3 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class Constants {}
+class Constants {
+  static String get url =>
+      dotenv.env['BASE_URL'] ??
+      "https://run.mocky.io/v3/c4ab4c1c-9a55-4174-9ed2-cbbe0738eedf";
+}
--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,1 +1,5 @@
-class Strings {}
+class Strings {
+  static const String errorMessage = "Failed to load Posts";
+  static const String noRows = "No Rows";
+  static const String searchHint = "Search";
+}
